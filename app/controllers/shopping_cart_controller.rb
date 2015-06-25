bb6f701280e03514c978cf30d8163daf707bc16c
samuelralak <samuelralak@ovi.com>
2015-06-26 02:04:01 +0300

    added toastr notification files

--- app/controllers/shopping_cart_controller.rb
+++ app/controllers/shopping_cart_controller.rb
@@ -1,7 +1,10 @@
 class ShoppingCartController < ApplicationController
 	def add_to_cart
 		@cart = Cart.create
-		@item = Item.find(params[:id])
+
+		if params[:id] 
+			@item = Item.find(params[:id])
+		end
 	end
 
   def remove_from_cart
