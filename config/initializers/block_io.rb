9ec814ee12a72f0bfec70dff82671ce4bd2f3c47
samuelralak <samuelralak@ovi.com>
2015-06-27 17:46:08 +0300

    configured env variables for bitcoin

--- config/initializers/block_io.rb
+++ config/initializers/block_io.rb
@@ -1,3 +1,3 @@
 require 'block_io'
 
-BlockIo.set_options :api_key => 'API KEY', :pin => 'SECRET PIN', :version => 2
+BlockIo.set_options :api_key => ENV['BLOCKIO_API_KEY'], :pin => ENV['BLOCKIO_SECRET_PIN'], :version => 2
