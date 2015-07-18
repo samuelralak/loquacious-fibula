6f645579db41aefcfb09366c7ac4e449e874587a
samuelralak <samuelralak@hotmail.com>
2015-07-18 11:47:17 +0300

    reverted to blockchain

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,6 @@ class User < ActiveRecord::Base
 	has_many :orders, foreign_key: "customer_id", dependent: :destroy
 	has_many :customers, through: :orders, source: :customer
 
-
 	after_create :create_btc_account
 
 	# Virtual attribute for authenticating by either username or email
@@ -34,15 +33,18 @@ class User < ActiveRecord::Base
 
 	private
 		def create_btc_account
-			label = SecureRandom.uuid
-			new_address = BlockIo.get_new_address(:label => label)
+			wallet = Blockchain::Wallet.new(ENV['BLOCKCHAIN_IDENTIFIER'], ENV['BLOCKCHAIN_PASSWORD'])
+			response = wallet.new_address(self.username)
 
-			if new_address && new_address['status'].eql?('success')
-				data = new_address['data']
+			unless response.nil?
+				# create account
+				account = BtcAccount.create(
+					label: response.label, address: response.address, user_id: self.id
+				)
 
-				# create new bitcoin account object
-				btc_account = BtcAccount.create(
-					label: data['label'], address: data['address'], user_id: self.id
+				# create account balance
+				account_balance = account.create_btc_account_balance(
+					available_balance: response.balance
 				)
 			end
 		end
