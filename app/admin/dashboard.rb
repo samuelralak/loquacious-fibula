643ce70b363d9256f264c65d9d7e5035c9405a04
samuelralak <samuelralak@hotmail.com>
2015-07-18 15:39:15 +0300

    same bug giving me trouble

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -28,10 +28,10 @@ ActiveAdmin.register_page "Dashboard" do
            panel "Seller Requests" do
              ul do
                SellerRequest.order('created_at desc').map do |seller_request|
-                 unless !seller_request.user && seller_request.user.try(:can_sell)
+                 if seller_request.user && !seller_request.user.can_sell
                     li "#{seller_request.user.try(:username)} | #{seller_request.user.try(:email)} | #{link_to('confirm', edit_admin_user_path(seller_request.user))}".html_safe
                  else
-                      "no seller requests"
+                    li "no seller requests"
                  end
                end
              end
