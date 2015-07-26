2183075cdd8fc7cf24ce171dcd4b80787ebdf0e2
samuelralak <samuelralak@hotmail.com>
2015-07-26 12:57:09 +0300

    making a post request to checkout

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,6 @@ Rails.application.routes.draw do
   get  '/buy',  	     to: 'items#buy',  			         as: 'buy_items'
   get  '/sell', 	     to: 'items#sell', 			         as: 'sell_items'
   get  '/deposit',     to: 'deposits#show',            as: 'deposit'
-  get  '/checkout',    to: 'order_events#checkout',    as: 'checkout'
   get  '/deposits',    to: 'deposits#index',           as: 'deposits'
   get  '/view_cart',   to: 'shopping_cart#view_cart',  as: 'view_cart'
   get  '/clear_cart',  to: 'shopping_cart#clear_cart', as: 'clear_cart'
@@ -23,6 +22,7 @@ Rails.application.routes.draw do
   get  '/blockchain_callback', to: 'application#blockchain_callback', as: 'blockchain_callback'
 
   post '/add_cards',        to: 'credit_cards#process_bin',       as: 'process_bin'
+  post  '/checkout',    to: 'order_events#checkout',    as: 'checkout'
   post '/send_coins',       to: 'deposits#send_coins',            as: 'send_coins'
   post '/add_to_cart',      to: 'shopping_cart#add_to_cart',      as: 'add_to_cart'
   post '/become_seller',    to: 'seller_request#create',          as: 'become_seller'
