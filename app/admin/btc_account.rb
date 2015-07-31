d6242312231b07c734091ab1d7ab320f8920bbc5
samuelralak <samuelralak@hotmail.com>
2015-07-31 13:48:44 +0300

    added bitcoin account balances

---
+++ app/admin/btc_account.rb
@@ -0,0 +1,24 @@
+ActiveAdmin.register BtcAccount do
+    index do
+        column :user
+        column :label
+        column :address
+        column :available_balance
+        column :pending_received_balance
+    end
+
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if resource.something?
+#   permitted
+# end
+
+
+end
