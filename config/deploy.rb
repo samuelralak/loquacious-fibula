b9b2694ccc748a68a56943a3225f2f965dd3625e
samuelralak <samuelralak@hotmail.com>
2015-08-08 00:22:13 +0300

    loadinf sidekiq quiet

--- config/deploy.rb
+++ config/deploy.rb
@@ -59,12 +59,12 @@ end
 
 # For capistrano 3
 namespace :sidekiq do
-    #task :quiet do
-        #on roles(:app) do
+    task :quiet do
+        on roles(:app) do
             # Horrible hack to get PID without having to use terrible PID files
-            #puts capture("kill -USR1 $(sudo initctl status workers | grep /running | awk '{print $NF}') || :")
-        #end
-    #end
+            puts capture("kill -USR1 $(sudo initctl status workers | grep /running | awk '{print $NF}') || :")
+        end
+    end
     task :restart do
         on roles(:app) do
             execute :sudo, :initctl, :restart, :workers
