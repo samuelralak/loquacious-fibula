63bd85adb4a9b913e3d67a535cb23d580d10e039
samuelralak <samuelralak@hotmail.com>
2015-08-08 01:03:49 +0300

    set up shared for sidekiq log and yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :repo_url, 'git@github.com:samuelralak/avtomat-kalashnikova.git'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml config/application.yml}
+set :linked_files, %w{config/database.yml config/application.yml config/sidekiq.yml log/sidekiq.log}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
