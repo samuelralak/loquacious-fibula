34eb9b8a3f58b9b1a0983f7997a0050fe1e16a45
samuelralak <samuelralak@hotmail.com>
2015-08-08 13:31:52 +0300

    now show balances in btc_account details

--- app/admin/btc_account.rb
+++ app/admin/btc_account.rb
@@ -1,10 +1,23 @@
 ActiveAdmin.register BtcAccount do
+    actions :all, except: [:create, :update, :destroy, :edit]
+
     index do
         column :user
         column :label
         column :address
-        column :btc_account_balance['available_balance']
-        column :btc_account_balance
+        actions
+    end
+
+    show do
+        attributes_table do
+            row :available_balance do
+                btc_account.btc_account_balance.available_balance
+            end
+            row :pending_received_balance do
+                btc_account.btc_account_balance.pending_received_balance
+            end
+        end
+        active_admin_comments
     end
 
 # See permitted parameters documentation:
