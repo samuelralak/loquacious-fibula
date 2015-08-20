b7515e1c4396842aab61227c9f1b891fa0ae8a63
samuelralak <samuelralak@hotmail.com>
2015-08-20 21:04:23 +0300

    fixed orders and reports

--- app/admin/order.rb
+++ app/admin/order.rb
@@ -3,10 +3,18 @@ ActiveAdmin.register Order do
 
 	index do
         selectable_column
+        column :item do |order|
+            "#{order.order_item.item.itemable.card_number}|#{order.order_item.item.itemable.expiry}|#{order.order_item.item.itemable.cvv}"
+        end
         column :order_total
         column :customer
-        column :created_at
+        column :seller do |order|
+            order.order_item.item.user.username
+        end
         column :order_status
+        column :reported do |order|
+            order.order_item.item.is_reported.eql?(true) ? link_to('YES', root_x_admi_n_report_path(order.order_item.item.report.id)) : "NO"
+        end
         actions
     end
 
