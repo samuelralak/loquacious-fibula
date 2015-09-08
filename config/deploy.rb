28684d9bdfcbc7c59a4a52b5d2d11ec25bf28f26
samuelralak <samuelralak@hotmail.com>
2015-09-08 23:02:33 +0300

    removed sidekiq tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -124,12 +124,12 @@ namespace :sidekiq do
     task :quiet do
         on roles(:app) do
             # Horrible hack to get PID without having to use terrible PID files
-            puts capture("kill -USR1 $(sudo initctl status workers | grep /running | awk '{print $NF}') || :")
+            # puts capture("kill -USR1 $(sudo initctl status workers | grep /running | awk '{print $NF}') || :")
         end
     end
     task :restart do
         on roles(:app) do
-            execute :sudo, :initctl, :restart, :workers
+            # execute :sudo, :initctl, :restart, :workers
         end
     end
 end
