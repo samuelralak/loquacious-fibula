6cb75be67e1f2a890aea273b0fe3075aa37bd4e9
samuelralak <samuelralak@hotmail.com>
2015-09-10 01:32:42 +0300

    trying to precompile assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,9 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
+# Precompile assets production
+Rails.config.assets.precompile = true
+
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
