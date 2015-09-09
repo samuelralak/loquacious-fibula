224183b7016d6f51e311c282dacd1516ad0c686a
samuelralak <samuelralak@hotmail.com>
2015-09-10 01:35:31 +0300

    fixed error

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,7 +4,7 @@
 Rails.application.config.assets.version = '1.0'
 
 # Precompile assets production
-Rails.config.assets.precompile = true
+Rails.application.config.assets.precompile = true
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
